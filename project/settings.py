f4a7637ed7640a0a2f5de51f8c3ebf9e119f237c
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-09 16:35:12 +0100

    fix cors

--- project/settings.py
+++ project/settings.py
@@ -202,9 +202,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000",
-#     "http://127.0.0.1:8000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
 import django_heroku
 django_heroku.settings(locals())
