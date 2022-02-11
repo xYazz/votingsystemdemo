fcb6e50e7563fa274dec26bccdfc2af522bed5ec
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-11 09:25:49 +0100

    fix loading page

--- frontend/src/components/LoadingPage.js
+++ frontend/src/components/LoadingPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Box from '@mui/material'
+import Box from '@mui/material/Box'
 import SpinStretch from 'react-cssfx-loading/lib/SpinStretch'
 const LoadingPage = () => {
   return (
