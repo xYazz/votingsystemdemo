ed71f34b9b755d7005b49d744c4c6ae8f6912577
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-13 14:04:51 +0100

    fix add candidate

--- project/settings.py
+++ project/settings.py
@@ -175,13 +175,13 @@ REST_FRAMEWORK = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
