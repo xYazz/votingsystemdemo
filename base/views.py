800951e76b86c82a7a3591ca23f0142c16f8e355
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-13 12:54:41 +0100

    fix add candidate

--- base/views.py
+++ base/views.py
@@ -263,7 +263,7 @@ class Results(APIView):
     def get(self,request, format=None, **kwargs):
         
         try: 
-            vote = Vote.objects.get(id=self.request.resolver_match.kwargs["pk"])
+            vote = Vote.objects.filter(id=self.request.resolver_match.kwargs["pk"])[0]
         except ObjectDoesNotExist:
             return Response({"Błąd", "Głosowanie nie istnieje."}, status=status.HTTP_404_NOT_FOUND)
         if len(VoteSerializer(vote).data['candidates']):
