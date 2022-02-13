21276209482c2cfd82973a58d8f671ce6d941457
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-13 13:40:39 +0100

    fix add candidate

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import MenuItem from '@material-ui/core/MenuItem';
-import { Chart, Series, Tooltip, Label, ArgumentAxis, Legend } from 'devextreme-react/chart'
+//import { Chart, Series, Tooltip, Label, ArgumentAxis, Legend } from 'devextreme-react/chart'
 import PieChart, {
     Series,
     Label,
