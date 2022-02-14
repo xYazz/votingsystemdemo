c20157c1fb5cd83a6273250fdc5eb48b42f74492
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-14 13:25:21 +0100

    optimizing app

---
+++ frontend/src/components/ConfirmDialog.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, TextField, Dialog, DialogActions, DialogContent, Text, DialogTitle}  from '@mui/material';
+import axiosInstance from '../axios';
+import { promptResponse } from './AddCandidates';
+import { useSnackbar } from 'notistack';
+
+const ConfirmDialog = (props) => {
+
+	const { enqueueSnackbar } = useSnackbar();
+
+    const handleConfirmClick = () => {
+        axiosInstance.delete(state.toDeleteURL + state.toDeleteId)
+          .then((res) => {
+            if (res.status == 202) {
+              promptResponse(enqueueSnackbar, 'Pomyślnie usunięto element.', 'success')
+            } else {
+    
+              promptResponse(enqueueSnackbar, 'Wystąpił nieznany błąd.', 'error')
+            }
+            
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+
+  return (
+    <div><Dialog
+    open={props.state.confirmDialogOpen}
+    TransitionComponent={Transition}
+    keepMounted
+    onClose={() => {
+        props.setState({ ...props.state, confirmDialogOpen: false });
+    }}
+    aria-describedby="alert-dialog-slide-description"
+  >
+    <DialogTitle>{"Uwaga"}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-slide-description">
+        Czy na pewno chcesz usunąć wybrany element?
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={() => {
+        props.setState({ ...props.state, confirmDialogOpen: false });
+      }}>Anuluj</Button>
+      <Button onClick={handleConfirmClick}>Potwierdź</Button>
+    </DialogActions>
+  </Dialog></div>
+  )
+}
+
+export default ConfirmDialog
