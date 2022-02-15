49c949ece65e5818a56850519f7a5f6d52e2b0fb
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-15 01:23:45 +0100

    small fixes

--- frontend/src/components/EditVote.js
+++ frontend/src/components/EditVote.js
@@ -6,7 +6,7 @@ import axiosInstance from '../axios';
 import { vote_type } from './CreateVotePage';
 import { promptResponse } from './AddCandidates';
 import { useSnackbar } from 'notistack';
-import { MenuItem, RadioGroup, FormControlLabel, FormLabel } from '@material-ui/core';
+import { MenuItem, RadioGroup, FormControl, FormControlLabel, FormLabel } from '@material-ui/core';
 
 const EditVote = (props) => {
     const { enqueueSnackbar } = useSnackbar();
