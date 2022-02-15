2ef9f36278ba522782d6507b00c51a60ff2d450e
Mateusz Więckowski <proyazz123@gmail.com>
2022-02-15 13:28:58 +0100

    optimize site

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: false,
   entry: "./frontend/src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
